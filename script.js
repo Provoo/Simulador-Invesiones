003e9122d125b4cfa6c38158ca8cf584c6094d2c
gdesign <design24439@gmail.com>
2015-06-26 00:26:50 -0500

    Se ha hecho que le algoritmo de interes compuesto funcione a la perfeccion

--- script.js
+++ script.js
@@ -4,23 +4,31 @@ var myApp = angular.module('myApp', []);
  myApp.controller('calculatorCtrl', function($scope) {
      
 
-   function rendimiento(montoInicial,rendimiento,dias){
+   function Calular_rendimiento(montoInicial,rendimiento,dias){
    	return (montoInicial*rendimiento/100*dias)/360;
    }
    
    function calcTotal(monto_inicial,monto_mensual, porcentaje, meses){
       var total_monto = monto_inicial;
-      var rendimiento = 0;
-      var total_rendimiento = 0;
+      var rendimiento = (total_monto*porcentaje/100)/12;
+      var total_rendimiento = rendimiento;
 
       for( var mes = 0; mes <=meses; mes++){
+        
+        if(mes === 0){
+         
+        }
+        else{
+          total_monto+=rendimiento+monto_mensual;
           rendimiento=(total_monto*porcentaje/100)/12;
           total_rendimiento +=rendimiento; 
-          total_monto+=rendimiento+monto_mensual;
-          $scope.objmeses.push({"mes" : (mes+1), "monto": total_monto, "rendimiento":rendimiento});
 
         }
-      return [total_monto,total_rendimiento];
+        
+        $scope.objmeses.push({"mes" : (mes+1), "monto": total_monto, "rendimiento":rendimiento});
+
+        }
+      return [total_monto+rendimiento,total_rendimiento];
     }
   function reset(){
       $scope.objmeses=[];
@@ -28,7 +36,6 @@ var myApp = angular.module('myApp', []);
   }
    
    $scope.calc = calcTotal;
-   $scope.calculador = rendimiento;
+   $scope.calculador = Calular_rendimiento;
    $scope.reset=reset;
    });  
-   
